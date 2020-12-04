6af6ad04e494c71c0186b3ba0d87d4cdaffe77b6
Frederik Riisager Johnsen <riisager@pop-os.localdomain>
2020-12-04 17:06:50 +0100

    edit

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -23,6 +23,7 @@ public class SetupTestUsers {
     User user = new User("user", "pass");
     User admin = new User("admin", "admin");
     User both = new User("user_admin", "joebiden");
+    User extDev = new User("extdev","2xhkxFYE");
 
     if(admin.getUserPass().equals("test")||user.getUserPass().equals("test")||both.getUserPass().equals("test"))
       throw new UnsupportedOperationException("You have not changed the passwords");
@@ -34,11 +35,13 @@ public class SetupTestUsers {
     admin.addRole(adminRole);
     both.addRole(userRole);
     both.addRole(adminRole);
+    extDev.addRole(userRole);
     em.persist(userRole);
     em.persist(adminRole);
     em.persist(user);
     em.persist(admin);
     em.persist(both);
+    em.persist(extDev);
     em.getTransaction().commit();
     System.out.println("PW: " + user.getUserPass());
     System.out.println("Testing user with OK password: " + user.verifyPassword("pass"));
